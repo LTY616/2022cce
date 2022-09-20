25ea1f89e73ae60931352610f8809ee254d92981
LTY616 <112918650+LTY616@users.noreply.github.com>
2022-09-20 12:00:47 +0800

    week03

---
+++ week03-3.cpp
@@ -0,0 +1,12 @@
+///要比大小(第二章 第三張)
+#include <stdio.h>
+int main()
+{
+    printf("請輸入數字n: ");
+
+    int n;
+    scanf("%d", &n);
+    if( n>10 ) printf("n>10");
+    if( n<10 ) printf("n<10");
+    if( n==10 ) printf("n==10");
+}

996a0ed2662ae153c68335672e4149af6c59d178
Lty0908065713 <ting33440713@gmail.com>
2022-12-06 12:09:56 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int sum(int a,int b);
+
+int main()
+{
+    ///在用之前，要先宣告declare 或 定義define
+    int ans = sum(2,3);///使用 呼叫
+    printf("ans: %d\n", ans);
+}
+int sum(int a,int b)///定義define
+{
+    return a+b;
+}///回傳 回覆人家
